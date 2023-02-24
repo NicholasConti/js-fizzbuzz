a4f8ccf65a45ee4369366805a406fad4630ca7b0
NicholasConti <conti94@live.it>
2023-02-24 11:07:21 +0100

    commenti

--- js/main.js
+++ js/main.js
@@ -2,27 +2,31 @@
 const ulElement = document.querySelector("ul.list");
 
 for (let i = 1; i <= 100; i++){
-    const liElement = document.createElement('li');
-    ulElement.append(liElement);
+    const liElement = document.createElement('li'); // ELEMENTO li X HTML
+    ulElement.append(liElement);    // INSERISCO 'li' NELL HTML
     switch (true){
+        //SE MULTIPLO DI 3 E 5
         case ((i % 3 === 0) && (i % 5 === 0)) :
             console.log('FizzBuzz');
-            liElement.append('FizzBuzz');
-            liElement.className = "box-both";
+            liElement.append('FizzBuzz'); // INSERISCO STRINGA NEI li
+            liElement.className = "box-both"; // ASEEGNO CLASSE AL li
             break;
+        // SE MULTIPLO DI 3
         case (i % 3 === 0) :
             console.log('Fizz');
-            liElement.append('Fizz');
-            liElement.className = "box-fizz";
+            liElement.append('Fizz');   // INSERISCO STRINGA NEI li
+            liElement.className = "box-fizz";   // ASEEGNO CLASSE AL li
             break;
+        // SE MULTIPLO DI 5
         case (i % 5 === 0) :
             console.log('Buzz');
-            liElement.append('Buzz');
-            liElement.className = "box-buzz";
+            liElement.append('Buzz');   // INSERISCO STRINGA NEI li
+            liElement.className = "box-buzz";   // ASEEGNO CLASSE AL li
             break;
+        // SE NON E MULTIPLO
         default:
             console.log(i);
-            liElement.append(i);
-            liElement.className = "box-std";
+            liElement.append(i);    // INSERISCO NUMERO i NEI li
+            liElement.className = "box-std";    // ASEEGNO CLASSE AL li
         }
     }
