8b3ecb540a42b48ace8845a8904866574cd1284a
NicholasConti <conti94@live.it>
2023-02-24 10:58:41 +0100

    fix1

--- js/main.js
+++ js/main.js
@@ -3,29 +3,26 @@ const ulElement = document.querySelector("ul.list");
 
 for (let i = 1; i <= 100; i++){
     const liElement = document.createElement('li');
+    ulElement.append(liElement);
     switch (true){
         case ((i % 3 === 0) && (i % 5 === 0)) :
             console.log('FizzBuzz');
             liElement.append('FizzBuzz');
-            ulElement.append(liElement);
             liElement.className = "box-both";
             break;
         case (i % 3 === 0) :
             console.log('Fizz');
             liElement.append('Fizz');
-            ulElement.append(liElement);
             liElement.className = "box-fizz";
             break;
         case (i % 5 === 0) :
             console.log('Buzz');
             liElement.append('Buzz');
-            ulElement.append(liElement);
             liElement.className = "box-buzz";
             break;
         default:
             console.log(i);
             liElement.append(i);
-            ulElement.append(liElement);
             liElement.className = "box-std";
         }
     }
